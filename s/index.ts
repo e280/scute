ff103619300d09ff9b265c096676f8e8e242e58f
Chase Moskal <chasemoskal@gmail.com>
2025-07-14 03:01:17 -0700

    export: exe fn

--- s/index.ts
+++ s/index.ts
@@ -1,5 +1,7 @@
 
 export * from "./ssg/tools/untab.js"
+export * from "./ssg/tools/resolver.js"
+export * from "./ssg/exe.js"
 export * from "./ssg/html.js"
 export * from "./ssg/io.js"
 export * from "./ssg/orb.js"
