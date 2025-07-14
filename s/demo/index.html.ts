0b5c78e7fb75c07e44e50fabb3dded543caf108c
Chase Moskal <chasemoskal@gmail.com>
2025-07-14 15:28:55 -0700

    fix: demo page

--- s/demo/index.html.ts
+++ s/demo/index.html.ts
@@ -6,7 +6,7 @@ import {partial} from "./stuff/partial.js"
 export default ssg.template(import.meta.url, async orb => html`
 	<html>
 		<head>
-			<script type=module src="${orb.hashurl("main.bundle.js")}"></script>
+			<script type=module src="${orb.hashurl("main.bundle.min.js")}"></script>
 			<link rel=stylesheet href="${orb.hashurl("main.css")}"/>
 			${orb.inject("main.css")}
 		</head>
