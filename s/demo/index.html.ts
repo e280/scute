2ed6e38cb3350a974a65d865268b4818d63cef8c
Chase Moskal <chasemoskal@gmail.com>
2025-09-06 14:38:52 -0700

    demo: improve self-testing demo dx

--- s/demo/index.html.ts
+++ s/demo/index.html.ts
@@ -8,19 +8,39 @@ export default ssg.template(import.meta.url, async orb => html`
 		<head>
 			<script type=module src="${orb.hashurl("main.bundle.min.js")}"></script>
 			<link rel=stylesheet href="${orb.hashurl("main.css")}"/>
-			${orb.inject("main.css")}
 		</head>
 		<body>
-			<h1>scute demo ${orb.packageVersion()}</h1>
+			<h1>scute demo v${orb.packageVersion()}</h1>
 
+			<h2>path tests</h2>
 			<div>
-				<p>/index.js hashurl ${orb.hashurl("/index.js")}</p>
-				<p>/index.js path ${orb.path("/index.js")}</p>
+				<p>
+					<strong>/index.js</strong>
+					<code>${orb.path("/index.js")}</code>
+				</p>
+				<p>
+					<strong>/index.js (hashurl)</strong>
+					<code>${orb.hashurl("/index.js")}</code>
+				</p>
+				<p>
+					<strong>../scute/types.js</strong>
+					<code>${orb.path("../scute/types.js")}</code>
+				</p>
+				<p>
+					<strong>$/x/scute/types.js</strong>
+					<code>${orb.path("$/x/scute/types.js")}</code>
+				</p>
 			</div>
 
+			<h2>partial test</h2>
 			<div class=partial>
 				${orb.place(partial)}
 			</div>
+
+			<h2>orb.inject test</h2>
+			<code>
+				${orb.inject("main.css")}
+			</code>
 		</body>
 	</html>
 `)
