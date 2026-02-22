d182cfb7264cf92863fef00190f51bef7f569033
Chase Moskal <chasemoskal@gmail.com>
2026-02-21 20:29:27 -0800

    export: ssg.emojiFavicon

--- s/ssg/ssg.ts
+++ s/ssg/ssg.ts
@@ -2,12 +2,12 @@
 import {page} from "./page.js"
 import {SocialCard} from "./types.js"
 import {exe, template} from "./template.js"
-import {darkreaderDisable, favicon, socialCard, utf8, viewport} from "./snippets.js"
+import {darkreaderDisable, emojiFavicon, favicon, socialCard, utf8, viewport} from "./snippets.js"
 
 export const ssg = {
 	template,
 	exe,
 	page,
-	meta: {utf8, viewport, darkreaderDisable, favicon, socialCard},
+	meta: {utf8, viewport, darkreaderDisable, favicon, emojiFavicon, socialCard},
 }
 
