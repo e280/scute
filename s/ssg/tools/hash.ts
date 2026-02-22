e624aa383772462e31121fe80bfa88aa8a7c6746
Chase Moskal <chasemoskal@gmail.com>
2026-02-21 21:27:29 -0800

    use: node's webcrypto explicitly

--- s/ssg/tools/hash.ts
+++ s/ssg/tools/hash.ts
@@ -1,9 +1,10 @@
 
 import {hex, txt} from "@e280/stz"
+import {webcrypto} from "node:crypto"
 
 export async function hash(text: string) {
 	const data = new Uint8Array(txt.toBytes(text))
-	const digest = await crypto.subtle.digest("SHA-256", data)
+	const digest = await webcrypto.subtle.digest("SHA-256", data)
 	return hex.fromBytes(new Uint8Array(digest))
 }
 
